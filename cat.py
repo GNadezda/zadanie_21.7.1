aef9cd3f2d114407b158a65681809e0e0aa977e2
unknown <grekovanadezda@mail.ru>
2023-04-28 19:04:16 +0300

    work

---
+++ cat.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def cat_name(self):
+        return self.name
+
+    def cat_gender(self):
+        return self.gender
+
+    def cat_age(self):
+        return self.age
---
+++ cat_2.py
@@ -0,0 +1,9 @@
+from cat import Cat
+
+cat_1 = Cat("Тишка", "мальчик", 5)
+cat_2 = Cat("Барсик", "мальчик", 3)
+
+print("Имя первого кота -", cat_1.cat_name())
+print("Возраст первого кота -", cat_1.cat_age())
+print("Пол второго кота -", cat_2.cat_gender())
+
